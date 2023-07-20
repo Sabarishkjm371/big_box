921af8a8b1dcea88ceef5343f238b9973bb80f01
Sabarishkjm371 <sabarishkjm371@gmail.com>
2023-07-20 11:43:56 +0530

    added comments

--- hello.py
+++ hello.py
@@ -1,9 +1,9 @@
-import numpy as np
-import pandas as pd
+import numpy as np	#importing
+import pandas as pd	#importing
 
 for i in range(5):
-	print("hello")
+	print("hello")	#printing hello
 
-dict_ = {"a":[1,2,3], "b":[5,6,7]}
-df = pd.DataFrame(dict_)
-print(df)
+dict_ = {"a":[1,2,3], "b":[5,6,7]}	#dictonary
+df = pd.DataFrame(dict_)		#data frame
+print(df)				#printing df
